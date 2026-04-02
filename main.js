476fb49fe370684f9e29834150cc15f57b375319
Frans <franeksokoliki73@gmail.com>
2026-04-02 23:27:51 +0200

    Remove hardcoded Firebase config
    Removed hardcoded Firebase configuration and added a comment about loading it from config.js.

--- main.js
+++ main.js
@@ -7,16 +7,8 @@
 // Firebase setup
 // -------------------------------------------------------------------------
 
-const firebaseConfig = {
-    apiKey:            'AIzaSyBeDzJgPfga58CNlEFriKkxVBG-d04JXO4',
-    authDomain:        'konsmon-website.firebaseapp.com',
-    databaseURL:       'https://konsmon-website-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId:         'konsmon-website',
-    storageBucket:     'konsmon-website.firebasestorage.app',
-    messagingSenderId: '1004639372000',
-    appId:             '1:1004639372000:web:49980358b5ac43526e8685',
-    measurementId:     'G-WJE8C8CY3E',
-};
+// Firebase config is loaded from config.js (see config.example.js for the required shape).
+// config.js is listed in .gitignore so credentials never enter the repository.
 
 firebase.initializeApp(firebaseConfig);
 
